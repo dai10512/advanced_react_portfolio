3f56625a3a78249bcff594e7c9bca0202ecde2b7
Daisuke Osanai <dsk.osanai@gmail.com>
2025-01-04 08:11:49 +0900

    Revert "Remove unused components and package.json: deleted Card component and package.json file to streamline the project structure and eliminate unnecessary dependencies."
    This reverts commit 404b349e8f60fefd8d741590b25b4090d1c3dced.

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,9 +2,8 @@ import { Heading, HStack, Image, Text, VStack } from "@chakra-ui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
-import { Card } from "@chakra-ui/react"
 
-const BuildCard = ({ title, description, imageSrc }:{
+const Card = ({ title, description, imageSrc }:{
   title: string,
   description: string,
   imageSrc: string
@@ -12,13 +11,7 @@ const BuildCard = ({ title, description, imageSrc }:{
   // Implement the UI for the Card component according to the instructions.
   // You should be able to implement the component with the elements imported above.
   // Feel free to import other UI components from Chakra UI if you wish to.
-  return (
-    <Card.Root width="320px">
-      {/* <Card.Header />
-      <Card.Body />
-      <Card.Footer /> */}
-    </Card.Root>
-  );
+  return null;
 };
 
-export default BuildCard;
+export default Card;
--- src/components/LandingSection.tsx
+++ src/components/LandingSection.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Avatar, Heading, Text, VStack } from "@chakra-ui/react";
+import { Avatar, Heading, VStack } from "@chakra-ui/react";
 import FullScreenSection from "./FullScreenSection";
 
 const greeting = "Hello, I am Pete!";
@@ -9,23 +9,14 @@ const bio2 = "specialised in React";
 // Implement the UI for the LandingSection component according to the instructions.
 // Use a combination of Avatar, Heading and VStack components.
 const LandingSection = () => (
-    <FullScreenSection
-      justifyContent="center"
-      alignItems="center"
-      isDarkBackground
-      backgroundColor="#2A4365"
-    >
-      <Avatar size="md" name="Sage" src=" https://i.pravatar.cc/150?img=7" />
-      <Heading as="h1" size="lg">
-        {greeting}
-      </Heading>
-      <Text as="p" fontSize="lg">
-        {bio1}
-      </Text>
-      <Text as="p" fontSize="xl">
-        {bio2}
-      </Text>
-    </FullScreenSection>
+  <FullScreenSection
+    justifyContent="center"
+    alignItems="center"
+    isDarkBackground
+    backgroundColor="#2A4365"
+  >
+    <></>
+  </FullScreenSection>
 );
 
 export default LandingSection;
